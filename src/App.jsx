d8054081bc697ba24391437068b20fe6d5f9dfbd
Evgenya Pankratova <janice0free@gmail.com>
2023-07-28 16:00:44 +0500

    change paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { Main } from "./components/Main/Main";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route} from "react-router-dom";
 import { NotFound } from "./components/NotFound/NotFound";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="container">
         <Routes>
           <Route path="/" element={<Main />} index />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
